a7e62918a0973dbf4485dc0ad546ead61a05b594
marwaZhr <zahar.marwa.13@gmail.com>
2021-12-11 21:26:39 +0100

    h

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
       <div className="centerPosition ">
         <img style={{ width: "200px" }} src="./images/logo1-01.png" alt="logo" />
       </div>
-      <div className="centerPosition ">
+      <div className="centerPosition pb-5">
         <p className="para ">
           Enfaite is a brand that represents singularity, passion and energy. <br />
           Our vision and identity are based and fueled by where we come from
@@ -33,7 +33,7 @@ export default function App() {
         <iframe className="embed-responsive-item " style={{ width: "500px" }} src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
       </div>
       <div className="centerPosition pt-3 pb-5">
-        <p style={{ color: "#e4341a" }}>GET YOUR ‘’ MAELSTROM ‘’ NOW, ON</p>
+        <p style={{ color: "#e4341a", fontWeight:"bold", fontSize:"1.3rem"}}>GET YOUR ‘’ MAELSTROM ‘’ NOW, ON</p>
         <div className="d-flex justify-content-center support">
           <a href="https://www.facebook.com/EnfaiteOfficial/" target="_blank"><FontAwesomeIcon icon={faFacebook} /></a>
           <a href="https://www.instagram.com/_enfaite/" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
